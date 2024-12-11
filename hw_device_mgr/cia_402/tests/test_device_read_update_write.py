b57153067b21b23c11956329469cf74f8e64dcb9
John Morris <john@zultron.com>
2024-12-11 16:23:28 -0600

    cia_402:  Add PV test case

--- hw_device_mgr/cia_402/tests/test_device_read_update_write.py
+++ hw_device_mgr/cia_402/tests/test_device_read_update_write.py
@@ -36,6 +36,9 @@ class TestCiA402DeviceRUW(_TestCiA301DeviceRUW, BaseCiA402TestClass):
     def test_read_update_write(self, obj):
         self.read_update_write_helper(obj, self.read_update_write_402_yaml)
 
+    def test_read_update_write_pv(self, obj):
+        self.read_update_write_helper(obj, "pv.cases.yaml")
+
 
 class TestCiA402DeviceRUWHMTimeout(TestCiA402DeviceRUW):
     def test_read_update_write(self, obj, mock_time):
