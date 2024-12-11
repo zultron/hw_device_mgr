3935862274463f96915316049b0bea9fdaa47c96
John Morris <john@zultron.com>
2024-12-11 16:41:31 -0600

    cia_402:  Remove velocity & torque request & success
    There's no strong need for `velocity_request` and `torque_request`,
    since PV and PT modes don't need the control word
    `OPERATION_MODE_SPECIFIC_1` bit set.  Note the
    `p[vt]_request_cw_flags()` functions don't actually set any control
    word flags.  It's also a little unexpected when `velocity_cmd` is
    non-zero, `velocity_fb` still matches cmd after `velocity_request` is
    cleared.
    
    The reason we *might* want them is to set the `control_mode` to
    `MODE_P[VT]` and not need to set `control_mode` manually.  We wanted
    that for `MODE_HM`, since the control word `OPERATION_MODE_SPECIFIC_1`
    bit must be set to actually start the home command, and it was simple
    for the robot, which is always in `MODE_CSP` except when in `MODE_HM`.
    There's less need for that with devices that deliberately switch
    between `MODE_PV` and `MODE_PT`, for example.  In this case, they
    should just be set directly through the control_mode HAL pin.

--- hw_device_mgr/cia_402/device.py
+++ hw_device_mgr/cia_402/device.py
@@ -274,7 +274,7 @@ class CiA402Device(CiA301Device, ErrorDevice):
             cm_str = self.control_mode_str(cm)
             cm_cmd_str = self.control_mode_str(cm_cmd)
             goal_reasons.append(f"control_mode {cm_str} != {cm_cmd_str}")
-        
+
         # Calculate 'state' feedback
         for state, bits in self.state_bits.items():
             # Compare masked status word with pattern to determine current state
