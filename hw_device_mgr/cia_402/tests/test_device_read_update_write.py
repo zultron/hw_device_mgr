dbb73f416febc6dec558ecf70dda2df0146e0a84
John Morris <john@zultron.com>
2024-12-12 12:02:36 -0600

    cia_402:  Add PT test case

--- hw_device_mgr/cia_402/tests/test_device_read_update_write.py
+++ hw_device_mgr/cia_402/tests/test_device_read_update_write.py
@@ -39,6 +39,9 @@ class TestCiA402DeviceRUW(_TestCiA301DeviceRUW, BaseCiA402TestClass):
     def test_read_update_write_pv(self, obj):
         self.read_update_write_helper(obj, "pv.cases.yaml")
 
+    def test_read_update_write_pt(self, obj):
+        self.read_update_write_helper(obj, "pt.cases.yaml")
+
 
 class TestCiA402DeviceRUWHMTimeout(TestCiA402DeviceRUW):
     def test_read_update_write(self, obj, mock_time):
